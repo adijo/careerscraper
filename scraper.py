5bf0f6822adb977128216129743f97859d47e64f
Aditya <1adityajoshi@gmail.com>
2013-10-14 21:18:54 +0530

    Scrapes all pages
    Still needs optimization. (Basic Version 1 Prototype)
    TODO
    1. Efficient processes
    2. Optimization functionality
    3. Not adding previously added questions.
    4. Prettifying and parsing questions to format them better.
    
    Ad on Changes:
    1. Clean questions.
    2. Improve question grammar.
    3. Output should be a PDF ready to be printed.

--- scraper.py
+++ scraper.py
@@ -8,6 +8,7 @@ class companyDownloader(object):
     
     def __init__(self,company_name):
         
+        self.pageNo = 1
         self.raw_url = "http://www.careercup.com/page?pid="
         self.company_name = company_name
         self.company_url = ""
@@ -15,6 +16,7 @@ class companyDownloader(object):
         for piece in pieces:
             self.company_url += piece + "-"
         self.final_url = self.raw_url + self.company_url + "interview-questions"
+        self.base_url = self.final_url
         self.database = shelve.open("database2.txt")
         self.file = open(company_name + ".txt","w")
         self.file.write(company_name.capitalize())
@@ -34,11 +36,12 @@ class companyDownloader(object):
         return urls
 
     def change_url(self,url):
-        return url + "&n=" + str(self.pageNo)
+        return self.base_url + "&n=" + str(self.pageNo)
 
     def travelToURL(self,question_url):
         soup = BeautifulSoup(urlopen(question_url).read())
         #Extract number of votes from the question
+        #TODO Partial Parser to format questions properly.
         votes = int(soup.find('div',{"class": 'votesNetQuestion'}).contents[0])
         
         if votes >= 0:
@@ -50,20 +53,41 @@ class companyDownloader(object):
             for line in raw_information:
                 try:
                     filtered = unicodedata.normalize('NFKD',line ).encode('ascii','ignore')
-                    self.file.write(filtered.capitalize())
+                    filtered = filtered.capitalize()
+                    print filtered
+                    self.file.write(filtered)
                 except:
                     self.file.write("\n")
+            print "--------"
             self.file.write("\n\n\n\n")       
             
             
         
     def driver_method(self):
+        print "Processing.."    
         list_of_urls = self.page_scraper(self.final_url)
-        
         for question in list_of_urls:
             q_url = "http://www.careercup.com" + str(question.contents[1]['href'])
             self.travelToURL(q_url)
+        self.pageNo += 1
+        self.final_url = self.change_url(self.final_url)
+        while True:
+            list_of_urls = self.page_scraper(self.final_url)
+            if len(list_of_urls) == 0:
+                break
+            else:
+                for question in list_of_urls:
+                    q_url = "http://www.careercup.com" + str(question.contents[1]['href'])
+                    self.travelToURL(q_url)
+                   
+                self.pageNo += 1
+                print "End of Page " + str(self.pageNo)
+                print "=========================================================="
+                
+                    
+        print "Done.."
+        self.file.close()
 
 
-d = companyDownloader("epic systems")
+d = companyDownloader("adobe")
 d.driver_method()
